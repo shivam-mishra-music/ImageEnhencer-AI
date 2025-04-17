f77e9d615d303e944aaaf64b20aad4f79e10f0c5
shivam-mishra-music <shivammishra.an5@gmail.com>
2025-04-17 19:18:47 +0530

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ImageEnhencerAI/',
+  base: '/ImageEnhencer-AI/',
   plugins: [react(), tailwindcss()],
 })
