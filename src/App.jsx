7b2d5db126bb85fbd4804bb1bad8948664bbddce
shivam-mishra-music <shivammishra.an5@gmail.com>
2026-04-26 22:20:18 +0530

    Remove footer text 'Powered By @SheryiansAI'
    Removed the 'Powered By @SheryiansAI' text from the footer.

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
             <Home />
 
             <div className="text-sm text-gray-500 mt-6">
-                Powered By @SheryiansAI
+                
             </div>
         </div>
     );
